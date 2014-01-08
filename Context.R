428cef88dbeb36bc40115f2bb2ea03bd268c1faa
Jussi Jousimo <jvj@iki.fi>
2014-01-09 04:02:12 +0700

    Update Context.R

--- Context.R
+++ Context.R
@@ -1,12 +1,27 @@
 Context <- setRefClass(
   "Context",
   fields = list(
+    sourceUrl = "character",
     processedDataDirectory = "ANY",
     scratchDirectory = "ANY"
   ),
   methods = list(
-    initialize = function(processedDataDirectory=NA, scratchDirectory=NA) {
-      callSuper(processedDataDirectory=processedDataDirectory, scratchDirectory=scratchDirectory)
+    initialize = function(sourceUrl="https://raw.github.com/statguy/R-Winter-Track-Counts/master/", processedDataDirectory=NA, scratchDirectory=NA) {
+      callSuper(sourceUrl=sourceUrl, processedDataDirectory=processedDataDirectory, scratchDirectory=scratchDirectory)
+    },
+    
+    loadSource = function(sourceFile) {
+      if (substr(sourceUrl, 1, 4) == "http") {
+        require(devtools)
+        src <- paste(sourceUrl, sourceFile, sep="/")
+        message("Loading source from ", src)
+        source_url(src)
+      }
+      else {
+        src <- file.path(sourceUrl, sourceFile)
+        message("Loading source from ", src)
+        source(src)
+      }
     }
   )
 )
