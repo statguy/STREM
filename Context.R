18c8de3c3e842ab44faeac2cb8cd5c0f4f73cb5a
Jussi Jousimo <jvj@iki.fi>
2014-01-09 14:31:39 +0700

    Update Context.R

--- Context.R
+++ Context.R
@@ -2,12 +2,14 @@ Context <- setRefClass(
   "Context",
   fields = list(
     sourceUrl = "character",
+    resultDataDirectory = "ANY",
     processedDataDirectory = "ANY",
+    rawDataDirectory = "ANY",
     scratchDirectory = "ANY"
   ),
   methods = list(
-    initialize = function(sourceUrl="https://raw.github.com/statguy/R-Winter-Track-Counts/master/", processedDataDirectory=NA, scratchDirectory=NA) {
-      callSuper(sourceUrl=sourceUrl, processedDataDirectory=processedDataDirectory, scratchDirectory=scratchDirectory)
+    initialize = function(sourceUrl="https://raw.github.com/statguy/R-Winter-Track-Counts/master/", resultDataDirectory=NA, processedDataDirectory=NA, rawDataDirectory=NA, scratchDirectory=NA) {
+      callSuper(sourceUrl=sourceUrl, resultDataDirectory=resultDataDirectory, processedDataDirectory=processedDataDirectory, rawDataDirectory=rawDataDirectory, scratchDirectory=scratchDirectory)
     },
     
     loadSource = function(sourceFile) {
@@ -22,6 +24,13 @@ Context <- setRefClass(
         message("Loading source from ", src)
         source(src)
       }
+    },
+    
+    getFileName = function(dir, name, response, region, ext=".RData") {
+      if (missing(response))
+        return(file.path(dir, paste(name, "-", region, ext, sep="")))
+      else
+        return(file.path(dir, paste(name, "-", response, "-", region, ext, sep="")))
     }
   )
 )
