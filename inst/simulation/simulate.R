4d02a6a875acc36a1f2dd198a43c4e79d2151c98
statguy <jvj@iki.fi>
2014-03-08 08:09:33 +0200

    ...

--- inst/simulation/simulate.R
+++ inst/simulation/simulate.R
@@ -3,9 +3,11 @@
 # Run full:
 # ./parallel_r.py -t 1:50 -n 50 -l 10.0 -b ~/tmp/blacklist.txt -v ~/git/Winter-Track-Counts/inst/simulation/simulate.R notest A
 
+err <- try({
 dryRun <- function(test) {
   context <- Context$new(resultDataDirectory=wd.data.results, processedDataDirectory=wd.data.processed, rawDataDirectory=wd.data.raw, scratchDirectory=wd.scratch, figuresDirectory=wd.figures)
   mss <- if (test == "test") MovementSimulationScenarioA$new(nAgents=as.integer(2), nIterations=as.integer(5), years=as.integer(2))$newInstance(context=context)
+  else MovementSimulationScenarioA$new()$newInstance(context=context)
 }
 
 simulateA <- function(test)
@@ -15,12 +17,13 @@ simulateA <- function(test)
   mss$simulateSingle(iteration=task_id)
 }
 
-err <- try({
   args <- commandArgs(trailingOnly=TRUE)
   if (length(args) != 3) stop("Invalid arguments.")
   test <- args[1]
   scenario <- args[2]
   task_id <- args[length(args)]
+  message("Arguments provided:")
+  print(args)
 
   library(parallel)
   library(doMC)
