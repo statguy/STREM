42b1b0c7603112300664dfcc8901e11a9665dc30
statguy <jvj@iki.fi>
2014-03-08 11:00:37 +0200

    ---

--- inst/simulation/count_intersections.R
+++ inst/simulation/count_intersections.R
@@ -1,12 +1,15 @@
 # Run test:
-# ./parallel_r.py -t 1:5 -n 2 -l 10.0 -b ~/tmp/blacklist.txt -v ~/git/Winter-Track-Counts/inst/simulation/count_intersections.R test
+# ./parallel_r.py -t 1:5 -n 2 -l 10.0 -b ~/tmp/blacklist.txt -v ~/git/Winter-Track-Counts/inst/simulation/count_intersections.R test A
 # Run full:
-# ./parallel_r.py -t 1:50 -n 50 -l 10.0 -b ~/tmp/blacklist.txt -v ~/git/Winter-Track-Counts/inst/simulation/count_intersections.R notest
+# ./parallel_r.py -t 1:50 -n 50 -l 10.0 -b ~/tmp/blacklist.txt -v ~/git/Winter-Track-Counts/inst/simulation/count_intersections.R notest A
+
+# library(devtools); install_github("statguy/Winter-Track-Counts")
 
 err <- try({
-  countIntersections <- function(iteration, test) {
+  countIntersections <- function(scenario, iteration, test) {
     context <- Context$new(resultDataDirectory=wd.data.results, processedDataDirectory=wd.data.processed, rawDataDirectory=wd.data.raw, scratchDirectory=wd.scratch, figuresDirectory=wd.figures)
-    mss <- MovementSimulationScenarioA$new()$newInstance(context=context)
+    mss <- if (scenario == "A") MovementSimulationScenarioA$new()$newInstance(context=context)
+    else stop("Unknown scenario ", scenario)
     study <- mss$study
     tracks <- study$loadTracks(iteration=iteration)
     surveyRoutes <- study$loadSurveyRoutes()
@@ -15,12 +18,17 @@ err <- try({
       intersections$findIntersections(tracks, surveyRoutes, dimension=1)
       intersections$saveIntersections()
     }
-    else message("SUCCESS")
+    else {
+      surveyRoutes$surveyRoutes <- surveyRoutes$surveyRoutes[1:2]
+      intersections$findIntersections(tracks, surveyRoutes, dimension=1)
+      message("SUCCESS")
+    }
   }
   
   args <- commandArgs(trailingOnly=TRUE)
-  if (length(args) != 2) stop("Invalid arguments.")
+  if (length(args) != 3) stop("Invalid arguments.")
   test <- args[1]
+  scenario <- args[2]
   task_id <- args[length(args)]
   message("Arguments provided:")
   print(args)
@@ -32,7 +40,7 @@ err <- try({
   library(WTC)
   source("~/git/Winter-Track-Counts/setup/WTC-Boot.R")
   
-  countIntersections(iteration=task_id, test=test=="test")
+  countIntersections(scenario=scenario, iteration=as.integer(task_id), test=test=="test")
 })
 
 if (inherits(err, "try-error")) {
