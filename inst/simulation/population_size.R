cb7a6df09c5a67469d941b0fe36ca41a12b28f77
statguy <jvj@iki.fi>
2014-04-13 12:57:44 +0700

    ...

--- inst/simulation/population_size.R
+++ inst/simulation/population_size.R
@@ -1,15 +1,19 @@
 # Run full:
 # ./parallel_r.py -t 1:50 -n 60 -l 10.0 -b ~/tmp/blacklist.txt -v ~/git/Winter-Track-Counts/inst/simulation/population_size.R notest A
+# R --vanilla --args notest A 1 < population_size.R
 
 # library(devtools); install_github("statguy/Winter-Track-Counts")
+# echo 'library(devtools); install_github("statguy/Winter-Track-Counts")' | R --slave
+# args <- c("notest","A","1")
 
 
 population_size <- function(mss, iteration, test) {
   if (test) {
     study <- mss$study
+    #iteration <- as.integer(50)
     estimates <- study$loadEstimates(iteration=iteration)
-    estimates$collectEstimates()
-    estimates$collectHyperparameters()
+    estimates$collectEstimates(predictionWeights=estimates$getPredictedOffset())
+    #estimates$collectHyperparameters()
     populationSize <- estimates$getPopulationSize(withHabitatWeights=mss$hasHabitatWeights())
     populationSize$loadValidationData()
     populationSize
@@ -19,9 +23,7 @@ population_size <- function(mss, iteration, test) {
   }
   else {
     study <- mss$study
-    estimates <- study$loadEstimates(iteration=iteration)
-    populationSize <- estimates$getPopulationSize(withHabitatWeights=mss$hasHabitatWeights())
-    populationSize$savePopulationSize()
+    study$getPopulationSize(iteration=iteration)
   }
 }
 
