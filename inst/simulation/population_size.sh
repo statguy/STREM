524efa96ac614edd895ffc5f19b33198b5895d5d
statguy <jvj@iki.fi>
2014-09-11 08:56:01 +0300

    ...

---
+++ inst/simulation/population_size.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+if [ -z "$1" ]
+then
+  echo "Missing parameter 'exec path'".
+  exit
+fi
+if [ -z "$2" ]
+then
+  echo "Missing parameter 'scenario'".
+  exit
+fi
+if [ -z "$3" ]
+then
+  echo "Missing parameter 'iterations'".
+  exit
+fi
+if [ -z "$4" ]
+then
+  echo "Missing parameter 'max_nodes'".
+  exit
+fi
+
+exec_path=$1
+scenario=$2
+iterations=$3
+max_nodes=$4
+count_days=1
+
+python "$exec_path"/parallel_r.py -t "$iterations" -n "$max_nodes" -l 10.0 -b ~/tmp/blacklist.txt -v ~/git/Winter-Track-Counts/inst/simulation/count_intersections.R notest "$scenario" "$count_days"
+reset
+python "$exec_path"/parallel_r.py -t "$iterations" -n "$max_nodes" -l 10.0 -b ~/tmp/blacklist.txt -v ~/git/Winter-Track-Counts/inst/simulation/estimate.R notest "$scenario"
+reset
+python "$exec_path"/parallel_r.py -t "$iterations" -n "$max_nodes" -l 10.0 -b ~/tmp/blacklist.txt -v ~/git/Winter-Track-Counts/inst/simulation/population_size.R notest "$scenario"
+reset
+python "$exec_path"/parallel_r.py -t "$iterations" -n "$max_nodes" -l 10.0 -b ~/tmp/blacklist.txt -v ~/git/Winter-Track-Counts/inst/simulation/validate.R notest "$scenario"
+reset
--- inst/simulation/simulate_tracks.sh
+++ inst/simulation/simulate_tracks.sh
@@ -24,3 +24,5 @@ count_days=1
 for scenario in A B C D E F; do
   python "$exec_path"/parallel_r.py -t "$iterations" -n "$max_nodes" -l 10.0 -b ~/tmp/blacklist.txt -v ~/git/Winter-Track-Counts/inst/simulation/simulate.R notest "$scenario"
 done
+
+# ./simulate_tracks.R ~/git/RParallelScreen/ 51:100 60
