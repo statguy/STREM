4a40cfa1cd655def0aa956ea0a944c75841d8464
statguy <jvj@iki.fi>
2014-09-11 09:32:53 +0300

    ...

--- inst/simulation/population_size.sh
+++ inst/simulation/population_size.sh
@@ -20,20 +20,26 @@ then
   echo "Missing parameter 'max_nodes'".
   exit
 fi
+if [ -z "$5" ]
+then
+  echo "Missing parameter 'model'".
+  exit
+fi
 
 exec_path=$1
 scenario=$2
 iterations=$3
 max_nodes=$4
+model=$5
 count_days=1
 
 python "$exec_path"/parallel_r.py -t "$iterations" -n "$max_nodes" -l 10.0 -b ~/tmp/blacklist.txt -v ~/git/Winter-Track-Counts/inst/simulation/count_intersections.R notest "$scenario" "$count_days"
 reset
-python "$exec_path"/parallel_r.py -t "$iterations" -n "$max_nodes" -l 10.0 -b ~/tmp/blacklist.txt -v ~/git/Winter-Track-Counts/inst/simulation/estimate.R notest "$scenario"
+python "$exec_path"/parallel_r.py -t "$iterations" -n "$max_nodes" -l 10.0 -b ~/tmp/blacklist.txt -v ~/git/Winter-Track-Counts/inst/simulation/estimate.R notest "$scenario" "$model"
 reset
-python "$exec_path"/parallel_r.py -t "$iterations" -n "$max_nodes" -l 10.0 -b ~/tmp/blacklist.txt -v ~/git/Winter-Track-Counts/inst/simulation/population_size.R notest "$scenario"
+python "$exec_path"/parallel_r.py -t "$iterations" -n "$max_nodes" -l 10.0 -b ~/tmp/blacklist.txt -v ~/git/Winter-Track-Counts/inst/simulation/population_size.R notest "$scenario" "$model"
 reset
-python "$exec_path"/parallel_r.py -t "$iterations" -n "$max_nodes" -l 10.0 -b ~/tmp/blacklist.txt -v ~/git/Winter-Track-Counts/inst/simulation/validate.R notest "$scenario"
+python "$exec_path"/parallel_r.py -t "$iterations" -n "$max_nodes" -l 10.0 -b ~/tmp/blacklist.txt -v ~/git/Winter-Track-Counts/inst/simulation/validate.R notest "$scenario" "$model"
 reset
 
-# ./population_size.sh ~/git/RParallelScreen/ Acombined 1:9 10
+# ./population_size.sh ~/git/RParallelScreen/ Acombined 1:9 10 FMPModel
