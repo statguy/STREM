dc0b3d5da99f823c5a331589181a7a9938054cfe
statguy <jvj@iki.fi>
2015-02-02 15:45:17 +0700

    -...

--- inst/simulation/all_all.sh
+++ inst/simulation/all_all.sh
@@ -1,4 +1,5 @@
 #!/bin/bash                                                                                                                            
 
+./count_intersections_all.sh
 ./estimate_all.sh
 ./population_size_all.sh
